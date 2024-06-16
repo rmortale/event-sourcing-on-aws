c8c185716f699cbfb6014d558205c9d982a8c979
Nino <dolce.antonio@gmail.com>
2024-06-16 18:42:09 +0200

    removed plugin config

--- cdk/reservation-infra/src/main/java/ch/dulce/resapp/infra/BootstrapApp.java
+++ cdk/reservation-infra/src/main/java/ch/dulce/resapp/infra/BootstrapApp.java
@@ -1,6 +1,9 @@
 package ch.dulce.resapp.infra;
 
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
 
 import java.util.Objects;
 
@@ -15,9 +18,19 @@ public class BootstrapApp {
         String accountId = (String) app.getNode().tryGetContext("accountId");
         Objects.requireNonNull(accountId, "please set accountId in context!");
 
+        Environment awsEnvironment = makeEnv(accountId, region);
 
-
+        Stack bootstrapStack = new Stack(app, "Bootstrap", StackProps.builder()
+                .env(awsEnvironment)
+                .build());
 
         app.synth();
+    }
+
+    static Environment makeEnv(String account, String region) {
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
     }
 }
